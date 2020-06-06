f16f97b03f670b65f5cbea4ae27daa20b21f21f8
Necati Unal <necati.unal01@gmail.com>
2020-06-06 19:52:54 +0200

    Initial commit

---
+++ spotify.py
@@ -0,0 +1,18 @@
+import argparser
+
+
+
+class Spotify():
+    # We want to get an URL from the command terminal (argparser)
+    # We need to make sure this is a playlist and contains music.
+    # For each of the songs, look it up in Spotify.
+    # Create a playlist in spotify
+    # Add the youtube playlist to the spotify playlist.
+
+
+    def main():
+        
+
+if __name__ == __main__:
+    spotify = Spotify()
+    s.main()
